02f7ea2ac92319d9c29ced17b223beac0f41cd19
bailasan65 <130461413+bailasan65@users.noreply.github.com>
2023-04-11 14:01:39 +0200

    first commit

---
+++ 1.py
@@ -0,0 +1,40 @@
+
+n="bpython rulEs!"
+print (n)
+print(n[:])
+print(n[::])
+print(n[0])
+print(n[1])
+print(n[-1])
+
+print(n[: : -1])
+print(n[: :-2])
+print(n[0:8])
+print(n[0::2])
+print(n[-13])
+
+print(len(n))
+print(n.upper())
+print(n)
+print(n.lower())
+print(n)
+print(n.swapcase())
+print(n.capitalize())
+
+print(n.find("ru"))
+print(n.rfind("ru"))
+
+name="Python Rules!"
+if "Rules" in name:
+      print("YES"
+)
+else:
+    print("NO")
+print(name.count("R"))    
+
+print(name.replace("R","BBB"))
+
+title="     This is the new title of   our book"
+print(title.strip())
+print(title.lstrip())
+print(title.rstrip())
